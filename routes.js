f191fe1d915ff56a1d3c164553d7c4c741f61a0a
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-09-07 18:07:50 -0300

    added loader

--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@ const router = require("express").Router()
 
 const authRoutes = require('./routes/authRoutes')
 const crudRoutes = require('./routes/crudRoutes')
+const indexRoutes = require('./routes/indexRoutes')
 
+router.use('/', indexRoutes)
 router.use('/', authRoutes)
 router.use('/', crudRoutes)
 
