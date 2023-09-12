164e6e8949e17427a4604e8c5dcf7fd471efec5b
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-09-12 16:47:31 -0300

    introduzindo metódo authenticador

--- models/authModel.js
+++ models/authModel.js
@@ -1,11 +1,18 @@
 module.exports = {
-  auth: () => {
-    console.log("Identicando a rota para exercer função.");
+  auth: (method) => {
+    switch (method) {
+      case "signup":
+        this.signup();
+      case "signin":
+        this.signin();
+      default:
+        return 404;
+    }
   },
   signup: () => {
     console.log("Identificando que se trata de um registro.");
   },
   signin: () => {
-    console.log("Identificando que se trata de um login")
+    console.log("Identificando que se trata de um login");
   },
 };
