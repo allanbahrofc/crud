d32935706fad7b2e2cdc05210eb47cb4671fdb7f
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-09-14 16:44:46 -0300

    storage method auth with handy-storage

--- models/storageModel.js
+++ models/storageModel.js
@@ -1,3 +1,29 @@
 const storageManager = require("handy-storage");
+const fileReader = require("fs");
+const { json } = require("body-parser");
 
-const storage = new storageManager({ beautify: true });
+const storage = new storageManager({
+  beautify: true,
+});
+
+storage.connect("../data/users.json");
+
+const setUserCredencial = (user, pass) => {
+  storage.setState({
+    user: {
+      username: user,
+      password: pass,
+    },
+  });
+};
+const getUserCredencial = () => {
+  fileReader.readFile("../data/users.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      jsonFile = JSON.parse(data);
+      return jsonFile.user;
+    }
+  });
+};
+(module.exports = setUserCredencial), getUserCredencial;
