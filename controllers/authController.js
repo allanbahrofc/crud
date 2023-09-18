705f8a4dc200235e3f6138d2e4ecfeedba4f8c54
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-09-18 13:35:22 -0300

    default credencials modified

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,6 @@ module.exports = (req, res) => {
   authModel
     .auth(req.params.method, req.body.txtUser, req.body.txtPass)
     .then((code) => {
-      res.send(code);
+      res.sendStatus(code);
     });
 };
